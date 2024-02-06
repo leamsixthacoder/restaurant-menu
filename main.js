cf585b9f8972133ef6dd6a973db3bfe419e7a29b
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-02-06 16:57:06 -0400

    adding more functionality

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 const menuArray = JSON.parse(localStorage.getItem('menus')) || [];
 const addedValues = [];
 
-const menuContainer = document.getElementById('menuContainer')
-const add = document.getElementById('add')
-const addMoreField = document.getElementById('add-more')
-const popup = document.querySelector('.pop-up')
-const save = document.getElementById('save')
+const menuContainer = document.getElementById('menuContainer');
+const add = document.getElementById('add');
+const addMoreField = document.getElementById('add-more');
+const popup = document.querySelector('.pop-up');
+const save = document.getElementById('save');
+const close = document.getElementById('close');
+const displayArea = document.getElementById('display-area');
+const foodMenuNameInput = document.getElementById('food-menu-name');
 
 loadMenus();
 
-add.addEventListener('click', addMenu)
-addMoreField.addEventListener('click', addMoreFields)
-save.addEventListener('click', addMenuList)
+add.addEventListener('click', () => popup.classList.remove('hidden'));
+addMoreField.addEventListener('click', addMoreFields);
+save.addEventListener('click', addMenuList);
+close.addEventListener('click', closePop);
 
 
 
-
-const displayArea = document.getElementById('display-area');
-
-
-function addMenu() {
-    popup.classList.remove('hidden')
+function editMenu() {
+    console.log('edit')
+}
+function closePop() {
+    document.querySelectorAll('.dynamic-fields input').forEach(input => (input.value = ''));
+    foodMenuNameInput.value = '';
+    popup.classList.add('hidden');
 }
 
 function addMoreFields() {
@@ -38,21 +43,17 @@ function addMoreFields() {
             price: priceValue
         });
 
-        document.querySelectorAll('.dynamic-fields input').forEach(input => input.value = '');
-    } else {
-
+        document.querySelectorAll('.dynamic-fields input').forEach(input => (input.value = ''));
     }
 }
 
-
 function addMenuList() {
-    const menuName = document.getElementById('food-menu-name').value
+    const menuName = foodMenuNameInput.value;
 
     if (menuName && addedValues.length > 0) {
         menuArray.push({
             name: menuName,
             menu: addedValues
-
         });
 
         // Save menuArray to local storage
@@ -61,42 +62,54 @@ function addMenuList() {
         // Reset addedValues for the next menu
         addedValues.length = 0;
 
-        // Append each menu to the menuContainer
-        loadMenus();
-
         // Clear the display area
         displayArea.innerHTML = '';
-        document.getElementById('food-menu-name').value = ''
-        menuContainer.innerHTML = '';
-        popup.classList.add('hidden');
-    } else {
 
+        // Clear the form
+        foodMenuNameInput.value = '';
+        document.querySelectorAll('.dynamic-fields input').forEach(input => (input.value = ''));
+
+        // Close the popup
+        popup.classList.add('hidden');
+
+        // Append each menu to the menuContainer
+        loadMenus();
     }
 }
 
 function loadMenus() {
     // Load existing menus from local storage
     const storedMenus = JSON.parse(localStorage.getItem('menus')) || [];
-    //create food menu container
-    storedMenus.forEach(menu => {
 
+    // Create food menu container
+    menuContainer.innerHTML = '';
+    storedMenus.forEach(menu => {
         const container = document.createElement('div');
-        container.className = 'food-menu'
+        container.className = 'food-menu';
         container.innerHTML = `
             <div class="menu-header">
-              <h2>${menu.name}</h2>
-              <span>RD$</span>
+                <div class="menu-header-action">
+                <h2>${menu.name}</h2>
+                <svg onclick="editMenu()" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z" />
+                <path d="M16 5l3 3" />
+                <path d="M9 7.07a7 7 0 0 0 1 13.93a7 7 0 0 0 6.929 -6" />
+                </svg>
+                </div>
+                <span>RD$</span>
             </div>
-            ${menu.menu.map(dish => `
-            <div class="menu-content">
-            <h3>${dish.dish}</h3>
-            <span>${dish.price}</span>
-          </div>
-            `).join('')}
-
-            `;
-        menuContainer.appendChild(container)
-
-    })
-
+            ${menu.menu
+                .map(
+                    dish => `
+                        <div class="menu-content">
+                            <h3>${dish.dish}</h3>
+                            <span>${dish.price}</span>
+                        </div>
+                    `
+                )
+                .join('')}
+        `;
+        menuContainer.appendChild(container);
+    });
 }
