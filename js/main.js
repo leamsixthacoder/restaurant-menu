d918ba3fe7606bd62c8afd65d616d73cf8045669
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-02-12 12:47:24 -0400

    delete edit icon

--- js/main.js
+++ js/main.js
@@ -86,13 +86,7 @@ function loadMenus() {
         container.innerHTML = `
             <div class="menu-header">
                 <div class="menu-header-action">
-                <h2>${menu.name}</h2>
-                <svg onclick="editMenu(${storedMenus.indexOf(menu)})" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z" />
-                <path d="M16 5l3 3" />
-                <path d="M9 7.07a7 7 0 0 0 1 13.93a7 7 0 0 0 6.929 -6" />
-                </svg>
+                <h2 onclick="editMenu(${storedMenus.indexOf(menu)})">${menu.name}</h2>
                 </div>
                 <span>RD$</span>
             </div>
@@ -101,6 +95,7 @@ function loadMenus() {
                     dish => `
                         <div class="menu-content">
                             <h3>${dish.dish}</h3>
+                            
                             <span>${dish.price}</span>
                         </div>
                     `
