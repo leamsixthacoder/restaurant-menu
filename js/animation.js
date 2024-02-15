e2bf9e2c487cd59b32f636a27993826a8b20beed
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-02-15 15:19:48 -0400

    implement reuse code for different sets of slides

--- js/animation.js
+++ js/animation.js
@@ -1,115 +1,87 @@
+function initializeSlider(slides, startIndex, intervalDuration, nextSlideFunction) {
+    let index = startIndex;
+    let intervalId = null;
+
+    function showSlide(index) {
+        if (index >= slides.length) {
+            index = 0;
+        } else if (index < 0) {
+            index = slides.length - 1;
+        }
+
+        slides.forEach(slide => {
+            slide.classList.remove('displaySlide');
+        });
+
+        slides[index].classList.add('displaySlide')
+    }
+
+    function nextSlide() {
+        index++
+        showSlide(index);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        if (slides.length > 0) {
+            slides[startIndex].classList.add('displaySlide');
+            intervalId = setInterval(nextSlideFunction, intervalDuration);
+        }
+    });
+
+    return {
+        showSlide,
+        nextSlide
+    };
+}
+
+
 
 //Image Slider
 
-const imageSlide = document.querySelectorAll('.slides-left img')
-let imageSlideIndex = 0
-let imageSlideintervalId = null;
-
-
-document.addEventListener('DOMContentLoaded', initializerSlide)
-
-function initializerSlide() {
-    if (imageSlide.length > 0) {
-        imageSlide[imageSlideIndex].classList.add('displaySlide')
-        imageSlideintervalId = setInterval(nextSlideImage, 5000)
-    }
-
-}
-
-function showSlideImage(index) {
-    if (index >= imageSlide.length) {
-        imageSlideIndex = 0
-
-    } else if (index < 0) {
-        imageSlideIndex = imageSlide.length - 1
-
-    }
-    imageSlide.forEach(slide => {
-        slide.classList.remove('displaySlide')
-    })
-    imageSlide[imageSlideIndex].classList.add('displaySlide')
-
-}
-
-function nextSlideImage() {
-    imageSlideIndex++
-    showSlideImage(imageSlideIndex)
-
-}
+const imageSlider = initializeSlider(
+    document.querySelectorAll('.slides-left img'),
+    0,
+    6000,
+    nextSlideImage
+)
 
 
 //Top Menu Slider
-const slidesMenuTop = document.querySelectorAll('.food-menu-static') 
-let slideMenuTopIndex = 0
-let intervalIdTop = null;
+const topMenuSlider = initializeSlider(
+    document.querySelectorAll('.food-menu-static'), 
+    0, 
+    9000, 
+    nextSlide
+)
 
+// Bottom Menu Slider
+const bottomMenuSlider = initializeSlider(
+    document.querySelectorAll('.food-menu'),
+    0,
+    9000,
+    nextSlideBottom
+);
+// Bottom Menu Slider Drinks
+const bottomMenuSliderDrinks = initializeSlider(
+    document.querySelectorAll('.food-menu-drinks'),
+    0,
+    9000,
+    nextSlideBottomDrinks
+);
 
-document.addEventListener('DOMContentLoaded', initializerSlideMenu)
-
-function initializerSlideMenu() {
-    if (slidesMenuTop.length > 0) {
-        slidesMenuTop[slideMenuTopIndex].classList.add('displaySlideMenu')
-        intervalIdTop = setInterval(nextSlide, 9000)
-    }
-}
-
-function showSlide(index) {
-    if (index >= slidesMenuTop.length) {
-        slideMenuTopIndex = 0
-
-    } else if (index < 0) {
-        slideMenuTopIndex = slidesMenuTop.length - 1
-
-    }
-    slidesMenuTop.forEach(slide => {
-        slide.classList.remove('displaySlideMenu')
-    })
-    slidesMenuTop[slideMenuTopIndex].classList.add('displaySlideMenu')
-
+function nextSlideImage() {
+    imageSlider.nextSlide();
 }
 
 
 function nextSlide() {
-    slideMenuTopIndex++
-    showSlide(slideMenuTopIndex)
-
-}
-
-//Bottom Menu Slider
-
-//Top Menu Slider
-const slidesMenuBottom = document.querySelectorAll('.food-menu') 
-let slideMenuBottomIndex = 0
-let intervalIdBottom = null;
-
-
-document.addEventListener('DOMContentLoaded', initializerSlideMenuBottom)
-
-function initializerSlideMenuBottom() {
-    if (slidesMenuBottom.length > 0) {
-        slidesMenuBottom[slideMenuBottomIndex].classList.add('displaySlideMenu')
-        intervalIdBottom = setInterval(nextSlideBottom, 9000)
-    }
-}
-
-function showSlideBottom(index) {
-    if (index >= slidesMenuBottom.length) {
-        slideMenuBottomIndex = 0
-
-    } else if (index < 0) {
-        slideMenuBottomIndex = slidesMenuBottom.length - 1
-
-    }
-    slidesMenuBottom.forEach(slide => {
-        slide.classList.remove('displaySlideMenu')
-    })
-    slidesMenuBottom[slideMenuBottomIndex].classList.add('displaySlideMenu')
-
+    topMenuSlider.nextSlide();
 }
 
 
 function nextSlideBottom() {
-    slideMenuBottomIndex++
-    showSlideBottom(slideMenuBottomIndex)
-
+    bottomMenuSlider.nextSlide();
+}
+function nextSlideBottomDrinks() {
+    bottomMenuSliderDrinks.nextSlide();
 }
