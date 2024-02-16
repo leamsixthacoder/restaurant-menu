f0a17dae2ec803abd6caf2a8fe6b87386bbac19d
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-02-16 18:02:50 -0400

    adding onclick event for the edit svg

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function openPopup() {
                 <path d="M14 11l0 6" />
                 <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
                 <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="24" height="24"
+              <svg xmlns="http://www.w3.org/2000/svg" onclick="editMenu(${storedMenus.indexOf(menus)})" class="icon icon-tabler icon-tabler-pencil" width="24" height="24"
                 viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
                 stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -182,6 +182,7 @@ function loadMenus() {
 
 
 function editMenu(index) {
+
     // Open modal
     popup.classList.remove('hidden');
     document.querySelector('.header-add-menu h3').innerHTML = 'Editar Menu'
